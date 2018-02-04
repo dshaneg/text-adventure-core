db64eae2ef2dbe327131cee192e488035dc029f2
Shane Gibbons <shane.gibbons@gmail.com>
2018-02-04 07:08:55 -0600

    Add a single dumb test

--- src/test/parsers/move-test.ts
+++ src/test/parsers/move-test.ts
@@ -1,1 +1,12 @@
 'use strict';
+
+import { expect } from 'chai';
+import 'mocha'
+
+describe('dummy test', () => {
+  describe('Increment', () => {
+    it('Should get 2 when adding 1 and 1.', () => {
+      expect(1+1).to.equal(2);
+    });
+  });
+});
