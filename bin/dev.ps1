45f0baa1a815a63fa196b3718574903a1c8db8d9
Shane Gibbons <shane.gibbons@gmail.com>
2018-02-04 04:08:46 -0600

    Call tsc in context of yarn

--- bin/dev.ps1
+++ bin/dev.ps1
@@ -1,8 +1,9 @@
 docker run `
   --rm `
   -it `
-  -w=/app `
+  -w=/src/text-adventure-core `
   --name adv-core-dev `
-  --mount type=bind,source="$pwd",target=/app `
-  node:8-alpine `
-  /bin/sh
+  --mount type=bind,source="F:\src",target=/src `
+  --mount type=volume,source="node_modules",target="/usr/local/lib/node_modules" `
+  dshaneg/nodejs:8-alpine `
+  /bin/bash
