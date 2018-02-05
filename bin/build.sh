5d5f1f5d64ace3344099e6cff2f734080ce20af1
Shane Gibbons <shane.gibbons@gmail.com>
2018-02-04 18:39:14 -0600

    Add coverage capability (that doesn't work yet)

--- bin/build.sh
+++ bin/build.sh
@@ -4,11 +4,12 @@ set -e
 
 source bin/shared.sh
 
-marquee "Lint Step"
-yarn lint
-
+# tslint wants you to make sure the code compiles before linting
 marquee "Transpile Step"
 yarn tsc
+
+marquee "Lint Step"
+yarn lint
 
 marquee "Unit Test Step"
 yarn test
--- src/test/parsers/move-test.ts
+++ src/test/parsers/move-test.ts
@@ -1,12 +1,12 @@
 'use strict';
 
 import { expect } from 'chai';
-import 'mocha'
+import 'mocha';
 
 describe('dummy test', () => {
   describe('Increment', () => {
     it('Should get 2 when adding 1 and 1.', () => {
-      expect(1+1).to.equal(2);
+      expect(1 + 1).to.equal(2);
     });
   });
 });
